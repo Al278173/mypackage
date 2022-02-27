dd90298e23e4262ca55e9d00d4f1fbe610866341
Ruyu Liu <50275025+Ruyul@users.noreply.github.com>
2022-02-27 14:55:45 -0600

    Added myncurve function

---
+++ R/myncurve.R
@@ -0,0 +1,19 @@
+#' @title myncurve
+#'
+#' @param mu mean value as a number
+#' @param sigma standard deviation as a number
+#' @param a a variable as a number
+#'
+#' @return A plot of a normal curve and the probability of X<=a
+#' @export
+#'
+#' @examples
+#' myncurve(mu=10, sigma=5, a=6)
+myncurve = function(mu, sigma, a){
+  curve(dnorm(x,mean=mu,sd=sigma), xlim = c(mu-3*sigma, mu +
+                                              3*sigma))
+  x=seq(mu-3*sigma, a, length=1000)
+  y=dnorm(x, mean=mu, sd=sigma)
+  polygon(c(mu-3*sigma, x, a), c(0, y, 0), col="Red")
+  list(area=round(pnorm(a, mean=mu, sd=sigma), 4))
+}
